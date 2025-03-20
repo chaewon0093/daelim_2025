a3c3f161d7106616529b88600e7c2e08e6581304
chaewon <cindybaek04@naver.com>
2025-03-20 12:34:55 +0900

    commit

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -1,35 +1,17 @@
+import 'package:daelim_2025/presentation/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
+  
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+    ),
+    home: const HomeScreen(),
+  );
 }
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-// MyHomePage 클래스 추가 (필요하면 수정 가능)
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const Center(child: Text('Hello, Flutter!')),
-    );
-  }
 }
