aed5a1a1315356ea27a8dd728b5a6b3c5316259b
chaewon <cindybaek04@naver.com>
2025-03-27 12:34:22 +0900

    0327

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-void main() {
-  runApp(const HomeScreen());
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0; // Declare a count variable
+
+  void _handleIncreaseCount() {
+    setState(() {
+      _count++; // Increment the count
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue, title: Text('Flutter Demo')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _handleIncreaseCount(); // Call the function to increase the count
+        },
+        child: Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('버튼 클릭시 횟수 증가', style: TextStyle(fontSize: 20)),
+            Text(
+              '$_count',
+              style: TextStyle(fontSize: 40), // Display the updated count
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
